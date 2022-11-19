ecdbc73335fa814a157cd1516324aab71e879d62
Karl Angelo Oriel <itsmelrak22@gmail.com>
2022-11-20 01:11:19 +0800

    added database

---
+++ db/db_hor.sql
@@ -0,0 +1,224 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 19, 2022 at 05:10 PM
+-- Server version: 8.0.28
+-- PHP Version: 8.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_hor`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `username` varchar(24) NOT NULL,
+  `password` varchar(191) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `name`, `username`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'Administrator', 'Admin', 'admin', NULL, NULL),
+(2, 'test', 'test', 'test', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guest`
+--
+
+CREATE TABLE `guest` (
+  `id` int NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `middlename` varchar(30) NOT NULL,
+  `lastname` varchar(40) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `contactno` varchar(13) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `guest`
+--
+
+INSERT INTO `guest` (`id`, `firstname`, `middlename`, `lastname`, `address`, `contactno`, `created_at`, `updated_at`) VALUES
+(1, 'sad', 'asdas', 'das', 'asdasd', 'asdas', NULL, NULL),
+(2, 'sad', 'asdas', 'das', 'asdasd', 'asdas', NULL, NULL),
+(3, 'asda', 'dsa', 'asds', 'adas', 'dasd', NULL, NULL),
+(4, 'asd', 'asdasd', 'asd', 'adas', 'asd', NULL, NULL),
+(5, 'asd', 'asdasd', 'asd', 'adas', 'asd', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE `room` (
+  `id` int NOT NULL,
+  `room_type` varchar(50) NOT NULL,
+  `price` varchar(11) NOT NULL,
+  `photo` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `room`
+--
+
+INSERT INTO `room` (`id`, `room_type`, `price`, `photo`) VALUES
+(1, 'Standard', '2000', '1.jpg'),
+(2, 'Superior', '2400', '3.jpg'),
+(3, 'Super Deluxe', '2800', '4.jpg'),
+(4, 'Jr. Suite', '3800', '5.jpg'),
+(5, 'Executive Suite', '4000', '6.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `transaction_id` int NOT NULL,
+  `guest_id` int NOT NULL,
+  `room_id` int NOT NULL,
+  `room_no` int NOT NULL,
+  `extra_bed` int NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `days` int NOT NULL,
+  `checkin` date NOT NULL,
+  `checkin_time` time NOT NULL,
+  `checkout` date NOT NULL,
+  `checkout_time` time NOT NULL,
+  `bill` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `id` int NOT NULL,
+  `guest_id` int NOT NULL,
+  `room_id` int NOT NULL,
+  `room_no` int DEFAULT NULL,
+  `extra_bed` tinyint NOT NULL DEFAULT '0',
+  `status` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `days` int DEFAULT NULL,
+  `checkin` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `checkin_time` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `checkout` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `checkout_time` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `bill` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `valid_until` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`id`, `guest_id`, `room_id`, `room_no`, `extra_bed`, `status`, `days`, `checkin`, `checkin_time`, `checkout`, `checkout_time`, `bill`, `valid_until`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 2, 3, 'Check Out', 2, '2022-11-16', '00:29:47', '2022-11-18', '16:29:52', '6400', '2022-11-15 17:22:32', NULL, NULL),
+(2, 3, 2, NULL, 0, 'Pending', NULL, '2022-11-19', NULL, '2022-11-21', NULL, NULL, '2022-11-15 17:22:42', NULL, NULL),
+(3, 4, 2, NULL, 0, 'Pending', NULL, '2022-11-24', NULL, '2022-11-26', NULL, NULL, '2022-11-15 15:22:03', NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `guest`
+--
+ALTER TABLE `guest`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`transaction_id`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `guest`
+--
+ALTER TABLE `guest`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `room`
+--
+ALTER TABLE `room`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `transaction_id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
